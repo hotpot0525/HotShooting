c1c584c24cca1a6b8f0ff3a3ae98aca103bc184d
Yuuki Takaesu <y-takaesu@cnt.biglobe.co.jp>
2012-12-05 12:36:01 +0900

    ターン制をより明確化

--- src/hotpot/game/hotshooting/GameScreen.java
+++ src/hotpot/game/hotshooting/GameScreen.java
@@ -75,53 +75,17 @@ public class GameScreen extends Screen {
 		turnOfPlayer();
 		turnOfEnemy();
 
-		// // プレイヤーの移動
-		// cPlayer.move();
+		gc();
 
-		// 自分の弾の生成
-		for (Bullet b : cPlayer.createBullet()) {
+		// ゲームカウント
+		frameCount++;
+		cPlayer.countUp();
+	}
 
-			pcBulletList.add(new BulletController(b, new BulletView(game
-					.getGraphics())));
-		}
-
-		// // 敵の生成
-		// // TODO コントローラで生成タイミングを決める
-		// if (frameCount % 100 == 0) {
-		// cEnemyList.add(new EnemyController(new Enemy(320, rand
-		// .nextInt(320 - 16)), new ViewEnemy(game.getGraphics())));
-		// }
-
-		// 自分の弾の移動
-		for (BulletController bc : pcBulletList) {
-			bc.move();
-		}
-
-		// // 自分の弾と敵の当たり判定
-		// for (Bullet b : bulletList) {
-		// if (b.state == Bullet.State.DIE) {
-		// continue;
-		// }
-		//
-		// for (EnemyController ec : cEnemyList) {
-		// if (ec.isHit(b)) {
-		// b.hitEnemy();
-		// score += ec.hitBullet();
-		// }
-		// }
-		// }
-		// // 敵の弾とプレイヤーの当たり判定
-		// for (BulletController b : cBulletList) {
-		// if (b.getState() == Bullet.State.DIE)
-		// continue;
-		//
-		// if (cPlayer.isHit(b)) {
-		// b.hit();
-		// cPlayer.hitBullet();
-		// }
-		//
-		// }
-
+	/**
+	 * 不要オブジェクトの削除
+	 */
+	private void gc() {
 		// 敵オブジェクトの破棄
 		for (int i = cEnemyList.size() - 1; i > -1; --i) {
 			if (cEnemyList.get(i).getState() != Enemy.State.ALIVE) {
@@ -144,7 +108,18 @@ public class GameScreen extends Screen {
 				cBulletList.remove(i);
 			}
 		}
+	}
 
+	/**
+	 * 敵の行動
+	 */
+	private void turnOfEnemy() {
+		// 敵の生成
+		// TODO コントローラで生成タイミングを決める
+		if (frameCount % 100 == 0) {
+			cEnemyList.add(new EnemyController(new Enemy(320, rand
+					.nextInt(320 - 16)), new ViewEnemy(game.getGraphics())));
+		}
 		// 敵の弾の生成
 		for (EnemyController ec : cEnemyList) {
 			ArrayList<Bullet> newBulletList = ec.createBullet();
@@ -158,29 +133,13 @@ public class GameScreen extends Screen {
 		for (EnemyController ec : cEnemyList) {
 			ec.move();
 		}
-
 		// 敵の弾の移動
 		for (BulletController bc : cBulletList) {
 			bc.move();
 		}
 
-		// ゲームカウント
-		frameCount++;
-		cPlayer.countUp();
-	}
-
-	/**
-	 * 敵の行動
-	 */
-	private void turnOfEnemy() {
-		// 敵の生成
-		// TODO コントローラで生成タイミングを決める
-		if (frameCount % 100 == 0) {
-			cEnemyList.add(new EnemyController(new Enemy(320, rand
-					.nextInt(320 - 16)), new ViewEnemy(game.getGraphics())));
-		}
-
 		checkHitToEnemyWithBullet(cEnemyList, pcBulletList);
+		checkHitToPlayerWithBullet(cPlayer, cBulletList);
 	}
 
 	/**
@@ -189,7 +148,20 @@ public class GameScreen extends Screen {
 	private void turnOfPlayer() {
 		// プレイヤーの移動
 		cPlayer.move();
+
+		// 自分の弾の生成
+		for (Bullet b : cPlayer.createBullet()) {
+
+			pcBulletList.add(new BulletController(b, new BulletView(game
+					.getGraphics())));
+		}
+
+		// 自分の弾の移動
+		for (BulletController bc : pcBulletList) {
+			bc.move();
+		}
 		checkHitToPlayerWithBullet(cPlayer, cBulletList);
+		checkHitToEnemyWithBullet(cEnemyList, pcBulletList);
 	}
 
 	/**
@@ -220,11 +192,6 @@ public class GameScreen extends Screen {
 	private void checkHitToEnemyWithBullet(
 			ArrayList<EnemyController> cEnemyList,
 			ArrayList<BulletController> pcBulletList) {
-		// 自分の弾と敵の当たり判定
-		// for (Bullet b : pcBulletList) {
-		// if (b.state == Bullet.State.DIE) {
-		// continue;
-		// }
 		for (BulletController b : pcBulletList) {
 			if (b.getState() == Bullet.State.DIE)
 				continue;
@@ -265,7 +232,8 @@ public class GameScreen extends Screen {
 		}
 
 		// HP
-		g.drawText("HP:" + Integer.toString(cPlayer.player.hp), 10, 10, Color.BLACK);
+		g.drawText("HP:" + Integer.toString(cPlayer.player.hp), 10, 10,
+				Color.BLACK);
 		// スコア
 		g.drawText("Score:" + Integer.toString(score), 100, 10, Color.BLACK);
 
